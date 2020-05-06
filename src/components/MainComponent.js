2cbcf5ac0ba0b14200a412f96d94bb01c79d54e9
Saurabh Mishra <saurabhmishra6179@gmail.com>
2020-05-06 22:59:01 +0530

    Functional components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -24,7 +24,7 @@ class Main extends Component {
             <div>
                 <Navbar dark color="primary">
                     <div className="container">
-                        <NavbarBrand href="/">Ristorance Con Fusion</NavbarBrand>
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
                 <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
